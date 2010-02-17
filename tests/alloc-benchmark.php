a53f6d67ff2b1fec8d56352967ac059c7540c6e9
Derick Rethans <derick@derickrethans.nl>
2010-02-17 22:34:45 +0000

    - Added a little memory-allocation benchmark script that show the difference   in speed between libc-alloc and zend-memory-manager-alloc.
    SVN Rev: 567

---
+++ tests/alloc-benchmark.php
@@ -0,0 +1,16 @@
+<?php
+$found = 0; $value = 0;
+$file = dirname( __FILE__ ) . "/primes.hash";
+
+$first = microtime( true );
+$hash = QuickHashIntHash::loadFromFile( $file );
+$second = microtime( true );
+unset( $hash );
+
+$third = microtime( true );
+$hash = QuickHashIntHash::loadFromFile( $file, QuickHashIntHash::DO_NOT_USE_ZEND_ALLOC );
+$fourth = microtime( true );
+
+echo $second - $first, "\n";
+echo $fourth - $third, "\n";
+?>
